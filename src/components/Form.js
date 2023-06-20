07ef8c13b5b3ffd270ae49d1a88672fd8d50e068
elnur004 <lnur004@gmail.com>
2023-06-21 03:14:29 +0400

    added logic to control form elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,32 @@
+import { useState } from 'react';
+
 const Form = () => {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(20);
+
   const submitHandler = (event) => {
     event.preventDefault();
   };
 
+  const inputHandler = (e) => setDescription(e.target.value);
+  const quantityHandler = (e) => setQuantity(Number(e.target.value));
+
   return (
     <form className="add-form" onSubmit={submitHandler}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
-        {Array.from({ length: 3 }, (_, i) => i + 1).map((num) => (
+      <select value={quantity} onChange={quantityHandler}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={inputHandler}
+      />
       <button>Add</button>
     </form>
   );
